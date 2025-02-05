cc66f7c7aa43b1f3c816c06d5406dc5c8687717c
Noor <noor.m@iksula.com>
2025-02-05 12:49:58 +0530

    magento ver 246 changes

--- Model/Resolver/Product/MediaGallery/Url.php
+++ Model/Resolver/Product/MediaGallery/Url.php
@@ -14,13 +14,12 @@ use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\GraphQl\Config\Element\Field;
 use Magento\Framework\GraphQl\Query\ResolverInterface;
 use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
-use Magento\Framework\ObjectManager\ResetAfterRequestInterface;
 use Pixelbinio\Pixelbin\Helper\Data as HelperData;
 
 /**
  * Returns media url
  */
-class Url implements ResolverInterface, ResetAfterRequestInterface
+class Url implements ResolverInterface
 {
     /**
      * @var ImageFactory
@@ -119,11 +118,4 @@ class Url implements ResolverInterface, ResetAfterRequestInterface
         return $image->getUrl();
     }
 
-    /**
-     * @inheritDoc
-     */
-    public function _resetState(): void
-    {
-        $this->placeholderCache = [];
-    }
 }
