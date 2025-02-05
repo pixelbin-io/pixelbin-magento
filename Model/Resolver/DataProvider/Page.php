8ee69886806042e87c5b6281a1538bd10a6fc43c
Noor <noor.m@iksula.com>
2025-02-05 10:45:35 +0530

    Schema graphql add

---
+++ Model/Resolver/DataProvider/Page.php
@@ -0,0 +1,159 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Pixelbinio\Pixelbin\Model\Resolver\DataProvider;
+
+use Magento\Cms\Api\Data\PageInterface;
+use Magento\Cms\Api\GetPageByIdentifierInterface;
+use Magento\Cms\Api\PageRepositoryInterface;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Widget\Model\Template\FilterEmulate;
+use Pixelbinio\Pixelbin\Helper\Data as HelperData;
+use Pixelbinio\Pixelbin\Logger\Logger;
+
+/**
+ * Cms page data provider
+ */
+class Page
+{
+    /**
+     * @var GetPageByIdentifierInterface
+     */
+    private $pageByIdentifier;
+
+    /**
+     * @var PageRepositoryInterface
+     */
+    private $pageRepository;
+
+    /**
+     * @var FilterEmulate
+     */
+    private $widgetFilter;
+
+    /**
+     * @var HelperData
+     */
+    protected $helperData;
+
+    /**
+     * @var Logger
+     */
+    protected $logger;
+
+    /**
+     * @param PageRepositoryInterface $pageRepository
+     * @param FilterEmulate $widgetFilter
+     * @param GetPageByIdentifierInterface $getPageByIdentifier
+     */
+    public function __construct(
+        PageRepositoryInterface $pageRepository,
+        FilterEmulate $widgetFilter,
+        GetPageByIdentifierInterface $getPageByIdentifier,
+        HelperData $helperData,
+        Logger $logger
+    ) {
+
+        $this->pageRepository = $pageRepository;
+        $this->widgetFilter = $widgetFilter;
+        $this->pageByIdentifier = $getPageByIdentifier;
+        $this->helperData = $helperData;
+        $this->logger = $logger;
+    }
+
+    /**
+     * Returns page data by page_id
+     *
+     * @param int $pageId
+     * @return array
+     * @throws NoSuchEntityException
+     */
+    public function getDataByPageId(int $pageId): array
+    {
+        $page = $this->pageRepository->getById($pageId);
+
+        return $this->convertPageData($page);
+    }
+
+    /**
+     * Returns page data by page identifier
+     *
+     * @param string $pageIdentifier
+     * @param int $storeId
+     * @return array
+     * @throws NoSuchEntityException
+     */
+    public function getDataByPageIdentifier(string $pageIdentifier, int $storeId): array
+    {
+        $page = $this->pageByIdentifier->execute($pageIdentifier, $storeId);
+
+        return $this->convertPageData($page);
+    }
+
+    /**
+     * Convert page data
+     *
+     * @param PageInterface $page
+     * @return array
+     * @throws NoSuchEntityException
+     */
+    private function convertPageData(PageInterface $page)
+    {
+        if (false === $page->isActive()) {
+            throw new NoSuchEntityException();
+        }
+
+        $renderedContent = $this->replaceImageUrl($this->widgetFilter->filter($page->getContent()));
+
+        $pageData = [
+            'url_key' => $page->getIdentifier(),
+            PageInterface::TITLE => $page->getTitle(),
+            PageInterface::CONTENT => $renderedContent,
+            PageInterface::CONTENT_HEADING => $page->getContentHeading(),
+            PageInterface::PAGE_LAYOUT => $page->getPageLayout(),
+            PageInterface::META_TITLE => $page->getMetaTitle(),
+            PageInterface::META_DESCRIPTION => $page->getMetaDescription(),
+            PageInterface::META_KEYWORDS => $page->getMetaKeywords(),
+            PageInterface::PAGE_ID => $page->getId(),
+            PageInterface::IDENTIFIER => $page->getIdentifier(),
+        ];
+        return $pageData;
+    }
+
+    public function replaceImageUrl($html)
+    {
+        if (!$this->helperData->isModuleEnabled()) {
+            return $html;
+        }
+        if (stripos($html, "&lt;img ") !== false) {
+            
+            $dom = new \domDocument();
+            $useErrors = libxml_use_internal_errors(true);
+            $dom->loadHTML($html);
+            libxml_use_internal_errors($useErrors);
+            $dom->preserveWhiteSpace = false;
+            $modified = 0;
+
+            preg_match_all('/img[^>]+g"/i', $html, $images);
+            foreach ($images[0] as $image) {
+                $secureImg = str_replace('img src="', '', $image);
+                $secureImg = str_replace('"', '', $secureImg);
+                
+                //$this->logger->info("Image URL = " . $secureImg);
+
+                $secureImg = $this->helperData->replaceCmsImageUrlWithPixelbin($secureImg);
+
+                $secureImg = '&lt;img src="'. $secureImg .'"';
+
+                $this->logger->info("Image URL = " . $secureImg);
+
+                $html = str_replace($image, $secureImg, $html);
+            }
+        }
+        return $html;
+    }
+}
---
+++ Model/Resolver/Product/MediaGallery/Url.php
@@ -0,0 +1,129 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Pixelbinio\Pixelbin\Model\Resolver\Product\MediaGallery;
+
+use Magento\Catalog\Model\Product;
+use Magento\Catalog\Model\Product\ImageFactory;
+use Magento\CatalogGraphQl\Model\Resolver\Products\DataProvider\Image\Placeholder as PlaceholderProvider;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Query\ResolverInterface;
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+use Magento\Framework\ObjectManager\ResetAfterRequestInterface;
+use Pixelbinio\Pixelbin\Helper\Data as HelperData;
+
+/**
+ * Returns media url
+ */
+class Url implements ResolverInterface, ResetAfterRequestInterface
+{
+    /**
+     * @var ImageFactory
+     */
+    private $productImageFactory;
+
+    /**
+     * @var PlaceholderProvider
+     */
+    private $placeholderProvider;
+
+    /**
+     * @var string[]
+     */
+    private $placeholderCache = [];
+
+    /**
+     * @var HelperData
+     */
+    protected $helperData;
+
+    /**
+     * @param ImageFactory $productImageFactory
+     * @param PlaceholderProvider $placeholderProvider
+     */
+    public function __construct(
+        ImageFactory $productImageFactory,
+        PlaceholderProvider $placeholderProvider,
+        HelperData $helperData
+    ) {
+        $this->productImageFactory = $productImageFactory;
+        $this->placeholderProvider = $placeholderProvider;
+        $this->helperData = $helperData;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function resolve(
+        Field $field,
+        $context,
+        ResolveInfo $info,
+        array $value = null,
+        array $args = null
+    ) {
+        if (!isset($value['image_type']) && !isset($value['file'])) {
+            throw new LocalizedException(__('"image_type" value should be specified'));
+        }
+
+        if (!isset($value['model'])) {
+            throw new LocalizedException(__('"model" value should be specified'));
+        }
+
+        /** @var Product $product */
+        $product = $value['model'];
+        if (isset($value['image_type'])) {
+            $imagePath = $product->getData($value['image_type']);
+
+            $imageFinalPath = $this->getImageUrl($value['image_type'], $imagePath);
+            if (!$this->helperData->isModuleEnabled()) {
+                return $imageFinalPath;
+            }
+            return $this->helperData->replaceProductImageUrlWithPixelbin($imageFinalPath);
+        } elseif (isset($value['file'])) {
+            $imageFinalPath = $this->getImageUrl('image', $value['file']);
+            if (!$this->helperData->isModuleEnabled()) {
+                return $imageFinalPath;
+            }
+            return $this->helperData->replaceProductImageUrlWithPixelbin($imageFinalPath);
+        }
+        return [];
+    }
+
+    /**
+     * Get image URL
+     *
+     * @param string $imageType
+     * @param string|null $imagePath
+     * @return string
+     * @throws \Exception
+     */
+    private function getImageUrl(string $imageType, ?string $imagePath): string
+    {
+        if (empty($imagePath) && !empty($this->placeholderCache[$imageType])) {
+            return $this->placeholderCache[$imageType];
+        }
+        $image = $this->productImageFactory->create();
+        $image->setDestinationSubdir($imageType)
+            ->setBaseFile($imagePath);
+
+        if ($image->isBaseFilePlaceholder()) {
+            $this->placeholderCache[$imageType] = $this->placeholderProvider->getPlaceholder($imageType);
+            return $this->placeholderCache[$imageType];
+        }
+
+        return $image->getUrl();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function _resetState(): void
+    {
+        $this->placeholderCache = [];
+    }
+}
