49b36afa6af7974877fa704b6a9be1fddf2a7241
Noor <noor.m@iksula.com>
2025-11-03 22:25:14 +0530

    Refactor code and remove duplicates

--- registration.php
+++ registration.php
@@ -1,15 +1,5 @@
 <?php
-/**
- * Iksula
- *
- * DISCLAIMER
- * Do not edit or add to this file if you wish to upgrade this extension to newer
- * version in the future.
- *
- * @category    Pixelbinio
- * @package     Pixelbinio_Pixelbin
- * @version     1.0.1
- */
+
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
     'Pixelbinio_Pixelbin',
